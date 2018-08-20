2f22b9490b79cee157c6a10e15c0f227eeb6c06f
Tom Koggel <tkoggel@signet.nl>
2018-08-20 20:12:23 +0200

    addition

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func handlerFunc1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome second site!</h1>")
+}
+
 func main() {
 	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/test", handlerFunc1)
 	http.ListenAndServe(":3000", nil)
 }
