08d6b5f5074c41adc64bacd4f1abbc0e609bb5c2
Tom Koggel <tkoggel@signet.nl>
2018-08-20 22:05:07 +0200

    Added an FAQ and custome 404 page!

--- main.go
+++ main.go
@@ -19,9 +19,22 @@ func contact(w http.ResponseWriter, r *http.Request) {
 		"support@test.test</a>.")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "This is a nice faq page!")
+}
+
+func sorry(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>I'm afraid i can't do that for you dave... ):")
+}
+
 func main() {
+	var nf http.Handler = http.HandlerFunc(sorry)
 	r := mux.NewRouter()
+	r.NotFoundHandler = nf
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
